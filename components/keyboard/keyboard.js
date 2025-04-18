481c7c365ddb3baf9f4a9eca1c0b56741eb91b62
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-04-18 16:30:24 +0800

    feature(keyboard): long press, change style

--- components/keyboard/keyboard.js
+++ components/keyboard/keyboard.js
@@ -5,7 +5,8 @@ Component({
       ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
       ['z', 'x', 'c', 'v', 'b', 'n', 'm']
     ],
-    inputValue: ''
+    inputValue: '',
+    timer: null
   },
 
   methods: {
@@ -16,7 +17,7 @@ Component({
       const key = e.currentTarget.dataset.key;
       let newValue = this.data.inputValue;
 
-      switch(key) {
+      switch (key) {
         case 'clear':
           newValue = '';
           break;
@@ -29,6 +30,37 @@ Component({
 
       this.setData({ inputValue: newValue });
       this.triggerEvent('inputchange', { value: newValue });
+    },
+    // 点击删除按钮
+    handleDeletePress() {
+      const that = this;
+      // 立即执行第一次删除
+      this.deleteHandel();
+      // 启动定时器
+      this.setData({
+        timer: setInterval(() => {
+          that.deleteHandel();
+        }, 100)
+      });
+    },
+    // 删除键松开/离开 清除定时器
+    handleDeleteRelease() {
+      clearInterval(this.data.timer);
+      this.setData({ timer: null });
+    },
+    // 删除字母逻辑
+    deleteHandel() {
+      let theVlaue= this.data.inputValue;
+      if (theVlaue.length > 0) {
+        wx.vibrateShort({
+          type: 'heavy'
+        })
+        theVlaue = theVlaue.slice(0,-1)
+        this.setData({
+          inputValue: theVlaue
+        });
+        this.triggerEvent('inputchange', { value: theVlaue });
+      }
     }
   }
 });
